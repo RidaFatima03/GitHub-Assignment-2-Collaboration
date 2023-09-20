cc0138a2a93ed096124074c4cd1be5dfdd9c219a
Parinaz <parinaz@ug.bilkent.edu.tr>
2023-09-20 16:26:41 +0330

    part b is done
    just make your name comment before writing your code section.

--- part2/part2.java
+++ part2/part2.java
@@ -1,7 +1,7 @@
 package part2;
-
+import java.util.Scanner;
 import java.util.Random;
-
+//Amina
 public class part2 {
     public static void main(String[] args) {
         int size = 10;
@@ -17,4 +17,37 @@ public class part2 {
 
             
     }
+    //Parinaz
+    public void displayMenu() {
+        System.out.println("Menu:");
+        System.out.println("1. Find minimum and maximum");
+        System.out.println("2. Find average and differences from average");
+        System.out.println("3. Sum of elements with odd and even indexes");
+        System.out.println("4. Exit");
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int choice;
+        do {
+            arrayOperations.displayMenu();
+            System.out.print("Enter your choice: ");
+            choice = scanner.nextInt();
+
+            if (choice == 1) {
+                arrayOperations.findMinMax();
+            } else if (choice == 2) {
+                arrayOperations.findAverageAndDifferences();
+            } else if (choice == 3) {
+                arrayOperations.sumOddAndEvenIndexes();
+            } else if (choice == 4) {
+                System.out.println("Exiting the program.");
+            } else {
+                System.out.println("Invalid choice. Please try again.");
+            }
+        } while (choice != 4);
+
+        scanner.close();
+    }
+    //Ilayda
+
 }
