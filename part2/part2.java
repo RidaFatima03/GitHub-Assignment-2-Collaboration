57f42e45fd6af2f48ce479ce436f0e629415be1f
İlayda Bozan <ilaydabozan90@gmail.com>
2023-09-20 16:15:31 +0300

    I added max-min methods

--- part2/part2.java
+++ part2/part2.java
@@ -50,4 +50,31 @@ public class part2 {
     }
     //Ilayda
 
+    public int findMaxNumber ( Array arr)
+    {
+        int maxNumber = ar[0];
+        for( int i = 0; i < arr.length; i++)
+        {
+            if( arr[i] > maxNumber )
+            {
+                maxNumber = arr[i];
+            }
+        }
+        return maxNumber;
+    }
+      
+    public int findMinNumber ( Array arr)
+    {
+        int minNumber = arr[0];
+        for( int i = 0; i < arr.length; i++)
+        {
+            if( arr[i] > minNumber )
+            {
+                minNumber = arr[i];
+            }
+        }
+        return minNumber;
+    }
+
+
 }
